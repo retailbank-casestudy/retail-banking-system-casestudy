0788df8d0bad386b9594adf38741f9249a766658
shinyjks <jjebashiny@gmail.com>
2020-12-29 11:49:09 +0530

    homepage comp and service added
    Made changes in module.ts

--- retail-banking-system/src/app/app.module.ts
+++ retail-banking-system/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+//let routes : Routes = [
+  //{path : "", component: },
+  //{path : "", component: },
+//];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //FormsModule,
+    //HttpClientModule,
+    //RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
