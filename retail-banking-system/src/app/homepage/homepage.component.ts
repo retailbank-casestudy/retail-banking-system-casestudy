5fdb0a2d0ba2d9ef54d61124c59a41ac3c6028d1
shinyjks <jjebashiny@gmail.com>
2021-01-02 14:35:20 +0530

    Homepage Updated

--- retail-banking-system/src/app/homepage/homepage.component.ts
+++ retail-banking-system/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomepageService } from '../homepage.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  username : any = undefined;
+  accounts : any = undefined;
+  transactions : any = undefined;
+  credits : any = undefined;
+  rewards : any = undefined;
+
+  constructor(private _service : HomepageService) { }
 
   ngOnInit(): void {
+    this._service.fetchAccounts().subscribe(response => this.accounts = response);
+    this._service.fetchTransactions().subscribe(response => this.transactions = response);
+    //this._service.fetchCredits().subscribe(response => this.credits = response);
+    //this._service.fetchRewards().subscribe(response => this.rewards = response);
   }
 
 }
